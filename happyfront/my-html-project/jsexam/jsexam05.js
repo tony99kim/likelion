6961f25bf77b3f06ea4aa75331d5a9e7d43c97dd
Taeyeop <tony69kim@naver.com>
2026-06-25 10:53:49 +0900

    jwt,swagger,security 실습

---
+++ happyfront/my-html-project/jsexam/jsexam05.js
@@ -0,0 +1,15 @@
+for(let i = 0 ; i <5; i++){
+    console.log(i);
+}
+
+const colors = ["red", "green", "blue"];
+for(let i = 0; i < colors.length; i++){
+    console.log(colors[i]);
+}
+
+
+let count = 0;
+while (count < 5) {
+    console.log(count);
+    count++;
+}
